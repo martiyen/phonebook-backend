c9f7a9113731c64be24c24d9a83fb402c9b7507a
martiyen <doyenmartin@gmail.com>
2025-01-29 17:19:24 +0100

    improved delete handler call

--- index.js
+++ index.js
@@ -50,9 +50,12 @@ app.get("/api/persons/:id", (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    request.params.id !== "undefined"? 
-    Person.deleteOne({ _id: request.params.id }).then(result => response.status(204).end()):
-    response.status(404).end()
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => response.status(204).end())
+    .catch(error => {
+        console.log('error:', error.message)
+        response.status(400).send(`error: ${error.message}`)
+    })
 })
 
 app.post('/api/persons', (request, response) => {
